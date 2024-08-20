51472894bc24e2d5e4fd1e7893959dc173741867
Victor Terra <victorbterra@gmail.com>
2024-08-20 13:21:28 -0300

    refactor:change name directories from database

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import publicRoutes from "./routes/public.js"
+import publicRoutes from "./routes/productRoutes/productRoutes.js"
 
 const app = express();
 const PORT = 3000 || 5000;
 
 app.use(express.json());
 
-app.use('/', publicRoutes)
+app.use(publicRoutes)
 
 app.listen(PORT, () => console.log(`Servidor funcionando na Porta ${PORT}`));
