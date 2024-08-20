4bd16245c2fd431b32a0cf30fef745383c6dce77
Victor Terra <victorbterra@gmail.com>
2024-08-20 12:49:32 -0300

    refactor:change local routes

--- server.js
+++ server.js
@@ -1,40 +1,11 @@
 import express from "express";
-import products from "./static/products.js";
+import publicRoutes from "./routes/public.js"
 
 const app = express();
+const PORT = 3000 || 5000;
 
 app.use(express.json());
 
-const PORT = 3000 || 5000;
-
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Pagina principal" });
-});
-
-app.get("/sobre", (req, res) => {
-  res.status(200).json({ message: "Rota sobre nós" });
-});
-
-app.get("/produtos", (req, res) => {
-  try {
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(404).json({ message: "Lista de produtos não encontrado." });
-  }
-});
-
-app.get("/produto/:id", (req, res) => {
-  const idProduct = req.params.id;
-  const findProduct = products.find((product) => product.id == idProduct);
-  try {
-    if (findProduct) {
-      res.status(200).json(findProduct);
-    }
-  } catch (error) {
-    res
-      .status(404)
-      .json({ message: "Não foi possível encontrar o produto desejado." });
-  }
-});
+app.use('/', publicRoutes)
 
 app.listen(PORT, () => console.log(`Servidor funcionando na Porta ${PORT}`));
